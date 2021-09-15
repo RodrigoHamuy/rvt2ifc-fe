355ef9820b916137c925fa30266418b95d81f21f
Rodrigo <rodrigo.hamuy@modulous.com>
2021-09-15 22:46:30 +0100

    publish

--- src/components/description/description.tsx
+++ src/components/description/description.tsx
@@ -7,15 +7,15 @@ const getDataList = (data: any) => {
 
     for (const k in data) {
         if (Object.prototype.hasOwnProperty.call(data, k)) {
-            if(!data[k]) {
-                console.log(k, data[k]); 
+            if (!data[k]) {
+                console.log(k, data[k]);
                 continue
             };
-            if(typeof data[k] === 'object') {
-                if(data[k].value) {
+            if (typeof data[k] === 'object') {
+                if (data[k].value) {
                     values.push([k, data[k].value]);
                 } else {
-                    console.log(k, data[k].value);                    
+                    console.log(k, data[k].value);
                 }
             } else {
                 values.push([k, data[k]]);
@@ -29,12 +29,64 @@ export const Description = () => {
 
     const { data } = useContext(R2iContext);
 
-    data && console.log(data);
+    // data && console.log(data);
 
     if (!data) return <></>;
+    
+    const rows : [string, string|number][] = [
+        ['Name', data.native.Name.value],
+        ['IFC type', data.native.constructor.name],
+        ['Object type', data.native.ObjectType.value],
+    ];
+
+    const dimensions = data.quantity.find((q:any)=>q.Name.value === 'Dimensions');
+
+    if(dimensions) {
+        const values = dimensions.HasProperties.map((v:any)=>[v.Name.value, v.NominalValue.value]);
+        console.log(...values);        
+        for (const v of values) {
+            if(v[0] === 'Area') v[1] = Math.round(v[1]*10000)/10000;
+            if(v[0] === 'Length') v[1] = Math.round(v[1]*10000);
+            if(v[0] === 'Volume') v[1] = Math.round(v[1]*1000)/1000;
+        }
+
+        rows.push(...values)
+    }
+
+    if(data.material.length) {
+        let layers;
+        switch (data.material[0].constructor.name) {
+            case 'IfcMaterial':
+                rows.push([`layer name`, data.material[0].Name.value])
+                break;
+            case 'IfcMaterialLayerSetUsage':
+                layers = data.material[0].ForLayerSet.MaterialLayers;
+                break;
+            case 'IfcMaterialLayerSet':
+                layers = data.material[0].MaterialLayers;
+                break;
+        }
+        if(layers) {
+            for (let i = 0; i < layers.length; i++) {
+                const m = layers[i];
+                rows.push([`layer ${i} name`, m.Material.Name.value])
+                rows.push([`layer ${i} thickness`, m.LayerThickness.value])
+            }
+        }
+    }
+    
 
     return <div className={classes.panel}>
-        <div className={classes.header}>native</div>
+        <div className={classes.header}>extracted</div>
+        <ul className={classes.ul}>
+            {rows.map((v, i) => (
+                <li key={i} className={classes.li}>
+                    <div>{v[0]}</div>
+                    <div className={classes.value}>{v[1]}</div>
+                </li>
+            ))}
+        </ul>
+        {/* <div className={classes.header}>native</div>
         <ul className={classes.ul}>
             {getDataList(data.native).map((v, i) => (
                 <li key={i} className={classes.li}>
@@ -45,7 +97,7 @@ export const Description = () => {
         </ul>
         <div className={classes.header}>material</div>
         <ul className={classes.ul}>
-            {data.material.flatMap((m: any)=>getDataList(m).map((v, i) => (
+            {data.material.flatMap((m: any) => getDataList(m).map((v, i) => (
                 <li key={i} className={classes.li}>
                     <div>{v[0]}</div>
                     <div className={classes.value}>{v[1]}</div>
@@ -54,12 +106,12 @@ export const Description = () => {
         </ul>
         <div className={classes.header}>quantity</div>
         <ul className={classes.ul}>
-            {data.quantity.flatMap((m: any, x: number)=>getDataList(m).map((v, i) => (
+            {data.quantity.flatMap((m: any, x: number) => getDataList(m).map((v, i) => (
                 <li key={`${x}${i}`} className={classes.li}>
                     <div>{v[0]}</div>
                     <div className={classes.value}>{v[1]}</div>
                 </li>
             )))}
-        </ul>
+        </ul> */}
     </div>
 }
---
+++ three-vs-babylon.md
@@ -0,0 +1,11 @@
+|              | babylon | threejs |
+| ------------ | ------- | ------- |
+| sizes        | 749 kb  | 122 kb  |
+| GitHub stars | 14.8k   | 74.2k   |
+| bvh          | no      | yes     |
+| lambert      | no      | yes     |
+
+code changes difference, time cost.
+
+merging strategy
+buffer instancing geometry
